64baec01ff917cb72c9549bc971b47323517f455
mr_bhanu__33 <115440020+Bhanuprakashvasa@users.noreply.github.com>
2023-05-15 20:45:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import Signup from './components/Signup'
+import Login from './components/Login'
+import {ForgetPassword,OTP }from './components/ForgetPassword'
+import {Route,Switch} from 'react-router-dom'
+import Home from './components/Home'
+import './App.css'
+import ResetPassword from './components/ResetPassword'
+
+
+function App() {
+
+  return (
+
+    <div className='App'>
+
+
+    <Switch>
+    <Route exact path='/'>
+    <Home/>
+    <Signup />
+    </Route>
+
+   
+    <Route exact path='/login'>
+    <Login/>
+    
+    </Route>
+    <Route exact path='/forgetpassword'>
+    <ForgetPassword/>
+    
+    </Route>
+    <Route exact path='/OTP'>
+    <OTP/>
+    
+    </Route>
+    <Route exact path='/resetpassword/:id'>
+    <ResetPassword/>
+    
+    </Route>
+    
+
+     
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
